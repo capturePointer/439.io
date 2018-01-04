fddff72b86de8ba2398df3db2194730fb6f39731
Yison Zhao <yisonzhao@gmail.com>
2018-01-04 14:01:31 +0800

    update logical

--- init.ddl.sql
+++ init.ddl.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `short_link` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `url` varchar(1024) NOT NULL DEFAULT '' COMMENT '原始url',
   `url_hash` char(32) NOT NULL DEFAULT '' COMMENT '原始url做md5，用于重复url记录检测',
   `created_at` int(11) NOT NULL DEFAULT '0',
--- short_link/c/IndexController.php
+++ short_link/c/IndexController.php
@@ -7,6 +7,16 @@ class IndexController extends Controller
 {
     public const ERROR_OK = 0;
     public const ERROR_ILLEGAL_URL = 1;
+    /**
+     * @var Mysqli_Database
+     */
+    protected $database;
+
+    public function __construct()
+    {
+        global $configs;
+        $this->database = Mysqli_Database::getIntance($configs['db']['mysqli']);
+    }
 
     public function index()
     {
@@ -19,7 +29,16 @@ class IndexController extends Controller
         $urlHashId = $segments[0];
         $hashId = new HashId();
         $id = $hashId->decode($urlHashId);
-        $url = 'query url from db where id';
+
+        $this->database->prepare("SELECT url FROM short_link WHERE `id` = ?;");
+        $this->database->execute($id);
+        $result= $this->database->results();
+        if (empty($result[0]['url'])) {
+            header('http/1.1 404');
+            return;
+        }
+
+        $url = $result[0]['url'];
         header('http/1.1 302');
         header('location: ' . $url);
     }
@@ -31,7 +50,21 @@ class IndexController extends Controller
             return $this->ajax([], self::ERROR_ILLEGAL_URL);
         }
 
-        $id = 'create db record';
+        $urlMd5 = md5($url);
+        $this->database->prepare("SELECT id FROM short_link WHERE `url_hash` = ?;");
+        $this->database->execute($urlMd5);
+        $result= $this->database->results();
+        if (!empty($result[0]['id'])) {
+            return $this->ajax([
+                'hash_id' => $result[0]['id'],
+            ]);
+        }
+
+        $id = $this->database
+            ->prepare("INSERT INTO `short_link` (url,url_hash, created_at) VALUES (?,?,?);")
+            ->execute($url, md5($url), time())
+            ->insert_id();
+
         $hashId = new HashId();
         $urlHashId = $hashId->encode($id);
         $data = [
@@ -46,11 +79,18 @@ class IndexController extends Controller
         $urlHashId = $segments[0];
         $hashId = new HashId();
         $id = $hashId->decode($urlHashId);
-        $url = 'db where id';
+
+        $this->database->prepare("SELECT url FROM short_link WHERE `id` = ?;");
+        $this->database->execute($id);
+        $result= $this->database->results();
+        if (!$result) {
+            header('http/1.1 404');
+            return;
+        }
+        $url = $result[0]['url'];
 
         $qrCode = new QrCode($url);
-
-        header('Content-Type: '.$qrCode->getContentType());
+        header('Content-Type: ' . $qrCode->getContentType());
         echo $qrCode->writeString();
     }
 
